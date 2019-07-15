9c8e4178c36ae06690a36030827a5604336af732
rodrigo-albuquerque <digofarias@gmail.com>
2019-07-15 16:26:47 +0100

    added app-pet-peers.go

--- statefulset/app-pet-peers.go
+++ statefulset/app-pet-peers.go
@@ -10,73 +10,121 @@ import (
 	"strings"
 )
 
-func checkError(err error) {
+func checkError(err error, message string) {
 	if err != nil {
+		log.Println(message)
 		log.Fatal(err)
 	}
 }
 
-func retrievePodSRVs(serviceName string) []*net.SRV {
-	_, srvs, err := net.LookupSRV("", "", serviceName)
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func getHostnames(serviceName string) []string {
+	// Receives an SRV record and Returns list of hostnames
+	_, hostNames, err := net.LookupSRV("", "", serviceName)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return srvs
+	var hosts []string
+	for _, host := range hostNames {
+		hosts = append(hosts, host.Target)
+	}
+	return hosts
 }
 
-func getPodData(url string) string {
+func getIPs(srvs []string, excludeLocalIP bool) []string {
+	// excludeLocalIP means local POD's IP will be excluded when true
+	var ips []string
+	for _, srv := range srvs {
+		if excludeLocalIP == false {
+			listOfIps, err := net.LookupIP(srv)
+			if err != nil {
+				log.Println("Failed lookup")
+				log.Fatal(err)
+			}
+			for _, ip := range listOfIps {
+				ips = append(ips, ip.String())
+			}
+		} else {
+			name, err := os.Hostname()
+			checkError(err, "Cannot retrieve hostname from OS")
+			if name != srv[:len(name)] {
+				listOfIps, err := net.LookupIP(srv)
+				if err != nil {
+					log.Println("Failed lookup")
+					log.Fatal(err)
+				}
+				for _, ip := range listOfIps {
+					ips = append(ips, ip.String())
+				}
+			}
+		}
+	}
+	return ips
+}
+
+func getRequestPod(url string) string {
+	// Makes GET request to URL and returns only reply's body
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return string(body)
+	requestBody, err := ioutil.ReadAll(resp.Body)
+	checkError(err, "Cannot not retrieve HTTP body message")
+	return string(requestBody)
 }
 
 func myhandler(resp http.ResponseWriter, req *http.Request) {
 	name, err := os.Hostname()
-	checkError(err)
+	checkError(err, "Cannot retrieve hostname from OS")
+	// POST requests are supposed to store data locally in /var/data/kubia.txt
 	if req.Method == "POST" {
 		postBody, err := ioutil.ReadAll(req.Body)
-		checkError(err)
+		checkError(err, "Cannot not retrieve HTTP body message")
 		// If file doesn't exist, create it, or append to file
 		f, err := os.OpenFile("/var/data/kubia.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		checkError(err)
+		checkError(err, "Cannot not open file")
 		defer f.Close()
 		n, err := f.WriteString(string(postBody) + "\n")
 		fmt.Printf("Wrote %d bytes\n", n)
 		fmt.Fprint(resp, "Data stored on pod "+name, "\n")
 	} else {
-		srvs := retrievePodSRVs("kubia")
-		name, err := os.Hostname()
-		checkError(err)
-		for _, srv := range srvs {
-			// If service is local, just respond directly
-			if strings.Contains(srv.Target, string(name)) {
-				// reply with pod's hostname
-				fmt.Fprint(resp, "You've hit "+name, "\n")
-				fmt.Fprint(resp, "Data stored on this pod: ")
-				// If kubia.txt exists, fwd its data, otherwise just say there is nothing in it
-				d, err := ioutil.ReadFile("/var/data/kubia.txt")
-				if err != nil {
-					fmt.Fprint(resp, "Data stored on this pod: No data posted yet\n")
-				} else {
-					fmt.Fprint(resp, string(d))
-				}
-				// Otherwise, if pod is remote, issue a GET request and print response
-			} else {
-				respBody := getPodData(srv.Target)
-				fmt.Fprint(resp, respBody)
+		// If GET request, we return our data
+		fmt.Fprint(resp, "You've hit "+name, "\n")
+		d, err := ioutil.ReadFile("/var/data/kubia.txt")
+		if err != nil {
+			fmt.Fprint(resp, "Data stored on this pod: No data posted yet\n")
+		} else {
+			fmt.Fprint(resp, "Data stored on this pod: \n")
+			fmt.Fprint(resp, string(d))
+		}
+		hosts := getHostnames("kubia")
+		ips := getIPs(hosts, false)
+		s := strings.Split(req.RemoteAddr, ":")
+		remoteIP, _ := s[0], s[1]
+		// This part is only executed if source address is NOT from one of the other pods
+		// i.e. don't request data from other pods if request comes from one of our pods
+		if sliceContains(ips, remoteIP) == false {
+			remoteIPs := getIPs(hosts, true)
+			for _, ip := range remoteIPs {
+				url := "http://" + ip + ":8080"
+				reqBody := getRequestPod(url)
+				fmt.Fprint(resp, reqBody)
 			}
 		}
 	}
 }
 
 func main() {
+	fmt.Println("Web server started on port 8080...")
 	http.HandleFunc("/", myhandler)
 	http.ListenAndServe(":8080", nil)
 }
